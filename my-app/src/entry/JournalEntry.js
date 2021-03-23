0871d1169223feb5d85ce6a93dec08b68f22296f
Calvin Corde <mail@calvincorde.com>
2021-03-23 12:26:23 +0100

    implemented localstorage to JournalEntry

--- my-app/src/entry/JournalEntry.js
+++ my-app/src/entry/JournalEntry.js
@@ -1,25 +1,56 @@
 import { Link } from "react-router-dom";
-import { useState } from 'react';
+// import saveToLocal from "./saveToLocal"
+import { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid'
 
 import './journalentry.css'
 
-export default function JournalEntry() {
+export default class JournalEntry extends Component  {
+  
+  state = {
+    headline: '',
+    entry: ''
+  };
 
-  const [entry, setEntry] = useLocalStorage (entry, setEntry);
+   handleChange = (event) => {
+    const input = event.target;
+    const value = input.value;
 
-  return (
-    <div>
-      <h2>This is Journal Entry</h2>
-      <input 
-        className="textbox" 
-        type="text" 
-        name="journalentry"
-        
-        />
-      <br></br>
-      <Link to="/summary/MainSummary">
-        <button>Finish</button>
-      </Link>
-  </div>
-  )
-}
+    this.setState({ [input.name]: value });
+  };
+
+  handleFormSubmit = () => {
+    const { headline, entry } = this.state;
+    localStorage.setItem( uuidv4(), JSON.stringify(this.state))
+  };
+    render() {
+      return (
+        <div>
+          <form onSubmit={this.handleFormSubmit}>
+            <h2>This is Journal Entry</h2>
+            <input 
+              className="textbox" 
+              type="text" 
+              name="headline"
+              placeholder="Headline"
+              value={this.state.user}
+              onChange={this.handleChange}
+              />
+            <input 
+              className="textbox" 
+              type="text" 
+              name="entry"
+              placeholder="Write a new entry"
+              value={this.state.user}
+              onChange={this.handleChange}
+              />
+              <button type="submit">Submit</button>
+            <br></br>
+            <Link to="/summary/MainSummary">
+              <button>Finish</button>
+            </Link>
+          </form>
+        </div>
+      )
+    }    
+  }
--- my-app/src/summary/MoodSummaryDetail.js
+++ my-app/src/summary/MoodSummaryDetail.js
@@ -2,10 +2,9 @@ export default function MoodSummaryDetail() {
     return (
         <div>
             <h3>This is Mood Summary Detail</h3>
-            <img src="/assets/img/radar_graph_placeholder.jpg" />
-            <img src="/assets/img/radar_graph_placeholder.jpg" />
-            <img src="/assets/img/radar_graph_placeholder.jpg" />
-            <img src="/assets/img/radar_graph_placeholder.jpg" />
+            <img src="/assets/img/radar_graph_placeholder.jpg" alt="This is a placeholder"/>
+            <img src="/assets/img/radar_graph_placeholder.jpg" alt="This is a placeholder"/>
+            <img src="/assets/img/radar_graph_placeholder.jpg" alt="This is a placeholder"/>
         </div>
     )
 }
