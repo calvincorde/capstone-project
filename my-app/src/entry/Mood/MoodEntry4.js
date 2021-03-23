7ca29350f26ae6e8e77e27e036a5c36d6e42d897
Calvin Corde <mail@calvincorde.com>
2021-03-23 15:34:50 +0100

    implemented localstorage to all moodentries

--- my-app/src/entry/Mood/MoodEntry4.js
+++ my-app/src/entry/Mood/MoodEntry4.js
@@ -1,15 +1,44 @@
 import { Link } from "react-router-dom";
+import { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid'
 
-export default function MoodEntry4() {
-  return (
-    <div>
-      <h2>This is Mood Entry</h2>
-      <div>
-        <input type="range" id="mood" name="moodslider" mind="1" max="10"></input>
-      </div>
-      <Link to="/entry/TagsEntry">
-        <button>Continue</button>
-      </Link>
-    </div>
-  )
+export default class MoodEntry4 extends Component {
+  
+  state = {
+    moodslider: ''
+  };
+
+   handleChange = (event) => {
+    const input = event.target;
+    const value = input.value;
+
+    this.setState({ [input.name]: value });
+  };
+
+  handleFormSubmit = () => {
+    const { moodRating } = this.state;
+    localStorage.setItem( 'MoodEntry4' + ' ' + uuidv4(), JSON.stringify(this.state))
+  };
+  
+  
+    render() {
+      return (
+        <form onSubmit={this.handleFormSubmit}>
+          <h3>This is Mood Entry 2</h3>
+            <input 
+            type="range" 
+            id="mood" 
+            name="moodslider" 
+            mind="1" 
+            max="10"
+            value={this.state.user}
+            onChange={this.handleChange}
+            />
+            <button type="submit">Submit</button>
+          <Link to="/entry/TagsEntry">
+              <button>Continue</button>
+            </Link>
+        </form>
+      )
+    }
 }
