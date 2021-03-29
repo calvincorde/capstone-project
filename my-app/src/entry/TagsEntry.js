57dcc34749547ff717ed50aaf1451d08172ee705
Calvin Corde <mail@calvincorde.com>
2021-03-29 15:43:03 +0200

    build a tag-component, did not save content to localstorage yet

--- my-app/src/entry/TagsEntry.js
+++ my-app/src/entry/TagsEntry.js
@@ -1,16 +1,49 @@
 import { Link } from "react-router-dom";
+import React from "react";
+import './TagsEntry.css'
 
 
 
 export default function TagsEntry() {
 
+  const [tags, setTags] = React.useState(["Tag1", "Tag2"]);
+  const removeTag =indextoRemove => {
+    setTags(tags.filter((_, index) => index !== indextoRemove));
+  };
+  const addTags = event => {
+    if (event.target.value !== "") {
+      setTags([... tags, event.target.value])
+      event.target.value = "";
+    }
+  };
+
   return (
     <div>
-      <h2>This is Tags Entry</h2>
+      <div className="tags-input">
+        <ul>
+          {tags.map((tag, index) => (
+              <li key={index} className="tag tags">
+              <span className="tag-title">{tag}</span>
+              <i 
+              className="material-icons tag-close-icon" 
+              onClick={() => removeTag(index)}
+              >close</i>
+            </li>
+          ))}
+        </ul>
+        <input 
+          type="text" 
+          placeholder="Enter your Tag here" 
+          onKeyUp={e => (e.key === "Enter" ? addTags(e) : null)} />
+      </div>
       <Link to="/entry/JournalEntry">
-        <button 
-        >Continue</button>
+            <button
+            // type="submit"
+            // onClick={this.handleFormSubmit}
+            className="master-text-text"
+            >continue</button>
       </Link>
-  </div>
+    </div>
   )
 }
+
